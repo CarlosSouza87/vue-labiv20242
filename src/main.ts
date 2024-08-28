fb6d0c258036f80f91ce1d322f6af9409a5604a0
Emanuel Mineda Carneiro <mineda@gmail.com>
2024-08-28 23:49:56 +0000

    Página de usuário e axios

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://8080-mineda-springbootlab420-0au4mp3yyrw.ws-us115.gitpod.io/'
+
 const app = createApp(App)
 
 app.use(createPinia())
