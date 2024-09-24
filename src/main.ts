14bb41ba6c6053cd3e4b5aac9c3b55dd59a135e5
Emanuel Mineda Carneiro <mineda@gmail.com>
2024-09-24 23:24:41 +0000

    Interceptor

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,17 @@ import App from './App.vue'
 import router from './router'
 
 import axios from 'axios'
+import { useUsuarioStore } from '@/stores/usuario';
 
 axios.defaults.baseURL = 'https://8080-mineda-springbootlab420-0au4mp3yyrw.ws-us116.gitpod.io/'
+
+axios.interceptors.request.use(config => {
+    const store = useUsuarioStore();
+    if(store.token) {
+      config.headers.Authorization = store.token
+    }
+    return config
+  })
 
 const app = createApp(App)
 
