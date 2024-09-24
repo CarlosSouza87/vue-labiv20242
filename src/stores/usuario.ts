236b5f4337321c2a49e0c9b6f663ec604ca06755
Emanuel Mineda Carneiro <mineda@gmail.com>
2024-09-24 23:17:11 +0000

    Pinia

---
+++ src/stores/usuario.ts
@@ -0,0 +1,29 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useUsuarioStore = defineStore('usuario', () => {
+  const nomeUsuario = ref<string>()
+  const token = ref<string>()
+  const erro = ref<string>()
+
+  async function login(nome:string, senha:string) {
+    try {
+        let response = (await axios.post('login', 
+                {
+                    nome: nome,
+                    senha: senha
+                }
+            ));
+        nomeUsuario.value = nome;
+        token.value = response.data.token;
+    }
+    catch(ex) {
+        erro.value = (ex as Error).message;
+    }
+  }
+
+  return { nomeUsuario, token, erro, login }},
+{ 
+    persist: true 
+})
